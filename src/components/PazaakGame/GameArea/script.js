23c24231845901db5649cf3b20e954f43c9644e1
stynan27 <seamustynan16@gmail.com>
2020-12-29 23:19:10 -0500

    Player Name Header

--- src/components/PazaakGame/GameArea/script.js
+++ src/components/PazaakGame/GameArea/script.js
@@ -1,11 +1,13 @@
 // @ is an alias to /src
 
+import PlayerHeader from '@/components/PazaakGame/GameArea/PlayerHeader/index'
 import OpponentGameArea from '@/components/PazaakGame/GameArea/OpponentGameArea/index'
 import PlayerGameArea from '@/components/PazaakGame/GameArea/PlayerGameArea/index'
 
 export default {
   name: 'GameArea',
   components: {
+    PlayerHeader,
     OpponentGameArea,
     PlayerGameArea
   },
