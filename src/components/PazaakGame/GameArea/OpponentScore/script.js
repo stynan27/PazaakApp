28e9c54a856fbc15a6bec474a0c494cee5197808
stynan27 <seamustynan16@gmail.com>
2020-12-30 16:48:48 -0500

    Player Rounds Won

---
+++ src/components/PazaakGame/GameArea/OpponentScore/script.js
@@ -0,0 +1,5 @@
+// @ is an alias to /src
+
+export default {
+  name: 'OpponentScore',
+}
--- src/components/PazaakGame/GameArea/script.js
+++ src/components/PazaakGame/GameArea/script.js
@@ -2,6 +2,10 @@
 
 import PlayerHeader from '@/components/PazaakGame/GameArea/PlayerHeader/index'
 import OpponentHeader from '@/components/PazaakGame/GameArea/OpponentHeader/index'
+import PlayerScore from '@/components/PazaakGame/GameArea/PlayerScore/index'
+import OpponentScore from '@/components/PazaakGame/GameArea/OpponentScore/index'
+import PlayerRoundsWon from '@/components/PazaakGame/GameArea/PlayerRoundsWon/index'
+import OpponentRoundsWon from '@/components/PazaakGame/GameArea/OpponentRoundsWon/index'
 import OpponentGameArea from '@/components/PazaakGame/GameArea/OpponentGameArea/index'
 import PlayerGameArea from '@/components/PazaakGame/GameArea/PlayerGameArea/index'
 
@@ -10,6 +14,10 @@ export default {
   components: {
     PlayerHeader,
     OpponentHeader,
+    PlayerScore,
+    OpponentScore,
+    PlayerRoundsWon,
+    OpponentRoundsWon,
     OpponentGameArea,
     PlayerGameArea
   },
