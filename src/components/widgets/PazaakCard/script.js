67ec84ffe2ce115172f86d9c704f6b863c296705
stynan27 <seamustynan16@gmail.com>
2021-07-19 10:33:29 -0400

    #1: Computed Pazaak card styling

--- src/components/widgets/PazaakCard/script.js
+++ src/components/widgets/PazaakCard/script.js
@@ -4,4 +4,27 @@
 
 export default {
   name: 'PazaakCard',
+  computed: {
+    backgroundColor() {
+      switch(this.cardType) {
+        case 'basic':
+          return 'basic-card-background';
+        case 'add':
+          return 'add-card-background';
+        case 'subtract':
+          return 'subtract-card-background';
+        default:
+          return 'special-card-background';
+      }
+    },
+    backgroundGradientColor() {
+      return this.backgroundColor + '-gradient';
+    },
+  },
+  data() {
+    return {
+      cardType: 'subtract',
+      cardValue: '-1',
+    }
+  },
 }
