2738ce0d276e9b8da6eb4006d1b77b834878933d
stynan27 <seamustynan16@gmail.com>
2021-07-21 00:33:11 -0400

    #1: Add icons to cards

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import Router from 'vue-router'
+import Plus from 'vue-material-design-icons/Plus.vue';
+import Minus from 'vue-material-design-icons/Minus.vue';
 
 import App from './App/index';
 import routes from './router/routes';
 
 Vue.use(Vuetify);
 Vue.use(Router);
+
+Vue.component('add-icon', Plus);
+Vue.component('minus-icon', Minus);
 
 new Vue({
   vuetify: new Vuetify({}),
