e464f873f330f5ca4b9beb1b139acfcde071c0af
stynan27 <seamustynan16@gmail.com>
2022-08-30 21:24:19 -0400

    Electron and socket packages #22

--- server/server.js
+++ server/server.js
@@ -1,34 +1,72 @@
-const express = require('express');
+const WebSocketServer = require('ws');
+
+//const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 // below for a .env config file
-// const path = require('path'); 
-const dotenv = require('dotenv').config();
+const path = require('path'); 
+// const dotenv = require('dotenv').config();
 
-if (dotenv.error) {
-  throw dotenv.error;
-}
+// if (dotenv.error) {
+//   throw dotenv.error;
+// }
 
-const app = express();
-const port = process.env.PORT;
+//const app = express();
+const wsPort = process.env.PORT || 3000; // process.env.PORT;
+
+// models
+// const hostRouter = require('./routes/host-router');
 
 // use statements (similar to include), need later?
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(cors());
+// app.use(bodyParser.json());
 
-// … Configure Express, and register necessary API route handlers
-
-// todo: separation between production and dev server
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(__dirname + '/../build'));
-
-//     app.get('*', (request, response) => {
-//         response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-//     });
-// } 
-
+// app url/uri rules (API) 
+// app.use('/', hostRouter);
 // app.use('/', playerRouter);
 // app.use('/', opponentRouter);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+let counter = 0;
+
+// process send example to show communication
+// setInterval(() => {
+//   process.send({ counter: counter++ });
+// }, 1000);
+
+const ws = new WebSocketServer.Server({
+  port: wsPort,
+});
+
+// Check for ws message types
+ws.on("connection", (socket) => {
+  console.log(`WebSocket connection established on port ${wsPort}`);
+  // send a message to the client
+  // socket.send(JSON.stringify({
+  //   type: "hello from server",
+  //   content: [ 1, "2" ]
+  // }));
+
+  // // receive a message from the client
+  // socket.on("message", (data) => {
+  //   const packet = JSON.parse(data);
+
+  //   switch (packet.type) {
+  //     case "hello from client":
+  //       // ...
+  //       break;
+  //   }
+  // });
+  socket.on("close", () => {
+    console.log("A client has disconnected");
+  });
+  
+  // handling client connection error
+  socket.on("error", () => {
+    console.error("Websocket Connection Error Occurred")
+  });
+
+  socket.on("message", (msg) => {
+    console.log('Msg recvd:', msg);
+  });
+});
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+        nodeIntegration: true
+    }
+  },
+  configureWebpack: {
+    // It will be merged into the final Webpack config
+    // plugins: [
+    //   ... // Your plugins here...
+    // ]
+    resolve: {
+      fallback: {
+        "fs": false,
+        "path": false,
+      } 
+    },
+  }
+})
